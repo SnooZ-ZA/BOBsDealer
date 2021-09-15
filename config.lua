ead19e59848eeb2b1dd2e9180b9306e86c54fed5
Snooz <60832206+SnooZ-ZA@users.noreply.github.com>
2021-09-15 03:21:53 +0200

    Add files via upload

--- config.lua
+++ config.lua
@@ -2,4 +2,4 @@ Config              = {}
 
 Config.callCops = 10  -- percent chance to call cops
 
-Config.dealerTime = 20 -- minutes a dealer stand and sell
+Config.dealerTime = 20 --time a dealer stand and sell
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,11 +6,13 @@ description 'Bobs Dealer'
 version '0.1'
 
 client_scripts {
+	'config.lua',
     'client/functions.lua',
     'client/main.lua'
 }
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
+	'config.lua',
     'server/main.lua'
 }
